148fbcbf62a2f1a8180067ea7a70a5dfc835aff7
dcep93 <dcep93@gmail.com>
2024-02-04 13:13:15 -0500

    x

--- throwbreak420/src/throwbreak420/Main.tsx
+++ throwbreak420/src/throwbreak420/Main.tsx
@@ -1,10 +1,19 @@
 export default function Main() {
   return (
-    <div>
+    <div style={{ height: "100vH", display: "flex", flexDirection: "column" }}>
       <div>config</div>
       <div>state</div>
-      <div>
-        <video autoPlay muted>
+      <div style={{ flexGrow: 1, position: "relative" }}>
+        <video
+          style={{
+            position: "absolute",
+            height: "100%",
+            maxWidth: "100%",
+          }}
+          autoPlay
+          muted
+          loop
+        >
           <source src={`video/grounded/1.mkv?${new Date()}`} />
         </video>
       </div>
