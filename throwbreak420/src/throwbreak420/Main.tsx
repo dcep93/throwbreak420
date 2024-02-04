11434e31c40dd44e6790179b565f4f521b83616c
dcep93 <dcep93@gmail.com>
2024-02-04 16:21:15 -0500

    x

--- throwbreak420/src/throwbreak420/Main.tsx
+++ throwbreak420/src/throwbreak420/Main.tsx
@@ -7,10 +7,10 @@ const ALL_MOVES = {
 };
 
 var initialized = false;
-var lastButton: string | null = null;
-const keyboardToButton: { [k: string]: string } = {};
 
 export default function Main() {
+  const [buttonToSet, updateButtonToSet] = useState("");
+  const keyboardToButton: { [k: string]: string } = {};
   const ref = createRef<HTMLVideoElement>();
   const backupRef = createRef<HTMLVideoElement>();
   const [isP1, updateIsP1] = useState(true);
@@ -18,11 +18,7 @@ export default function Main() {
   const [moves, updateMoves] = useState(
     Object.fromEntries(Object.keys(ALL_MOVES).map((k) => [k, true]))
   );
-  const [speed, _updateSpeed] = useState(1);
-  const updateSpeed = (s: number) => {
-    _updateSpeed(s);
-    ref.current!.playbackRate = s;
-  };
+  const [speed, updateSpeed] = useState(1);
   const [move, updateMove] = useState("");
   const prepRandom = () => {
     const choices = Object.entries(moves)
@@ -45,126 +41,133 @@ export default function Main() {
     <div
       tabIndex={1}
       onKeyDown={(e) => {
-        if (keyboardToButton[e.key] === undefined) {
-          if (lastButton === null) return;
-          keyboardToButton[e.key] = lastButton;
+        const button = keyboardToButton[e.key];
+        if (button === undefined) {
+          updateButtonToSet("1");
         }
         breakThrow(keyboardToButton[e.key]);
       }}
-      style={{
-        height: "100vH",
-        display: "flex",
-        flexDirection: "column",
-        fontFamily: "Courier New",
-      }}
     >
-      <div>
-        <form
-          style={{ display: "flex", justifyContent: "space-around" }}
-          onSubmit={(e) => e.preventDefault()}
-        >
-          <div>
+      <div
+        style={{
+          height: "100vH",
+          display: "flex",
+          flexDirection: "column",
+          fontFamily: "Courier New",
+        }}
+      >
+        <div>
+          <form
+            style={{ display: "flex", justifyContent: "space-around" }}
+            onSubmit={(e) => e.preventDefault()}
+          >
             <div>
-              <label>
-                <input
-                  type="radio"
-                  name="p1"
-                  checked={isP1}
-                  onChange={() => updateIsP1(true)}
-                />
-                p1
-              </label>
-            </div>
-            <div>
-              <label>
-                <input
-                  disabled // TODO
-                  type="radio"
-                  name="p1"
-                  checked={!isP1}
-                  onChange={() => updateIsP1(false)}
-                />
-                p2
-              </label>
-            </div>
-          </div>
-          <div>
-            <div>
-              <label>
-                <input
-                  type="radio"
-                  name="grounded"
-                  checked={isGrounded}
-                  onChange={() => updateIsGrounded(true)}
-                />
-                grounded
-              </label>
-            </div>
-            <div>
-              <label>
-                <input
-                  type="radio"
-                  name="grounded"
-                  checked={!isGrounded}
-                  onChange={() => updateIsGrounded(false)}
-                />
-                standing
-              </label>
-            </div>
-          </div>
-          <div>
-            {Object.entries(ALL_MOVES).map(([k, v]) => (
-              <div key={k}>
+              <div>
                 <label>
                   <input
-                    type={"checkbox"}
-                    checked={moves[k]}
-                    onChange={() =>
-                      updateMoves(Object.assign({}, moves, { [k]: !moves[k] }))
-                    }
+                    type="radio"
+                    name="p1"
+                    checked={isP1}
+                    onChange={() => updateIsP1(true)}
                   />
-                  {v}
+                  p1
                 </label>
               </div>
-            ))}
-          </div>
-          <div>
-            <div>speed: {speed.toFixed(2)}</div>
+              <div>
+                <label>
+                  <input
+                    disabled // TODO
+                    type="radio"
+                    name="p1"
+                    checked={!isP1}
+                    onChange={() => updateIsP1(false)}
+                  />
+                  p2
+                </label>
+              </div>
+            </div>
             <div>
+              <div>
+                <label>
+                  <input
+                    type="radio"
+                    name="grounded"
+                    checked={isGrounded}
+                    onChange={() => updateIsGrounded(true)}
+                  />
+                  grounded
+                </label>
+              </div>
+              <div>
+                <label>
+                  <input
+                    type="radio"
+                    name="grounded"
+                    checked={!isGrounded}
+                    onChange={() => updateIsGrounded(false)}
+                  />
+                  standing
+                </label>
+              </div>
+            </div>
+            <div>
+              {Object.entries(ALL_MOVES).map(([k, v]) => (
+                <div key={k}>
+                  <label>
+                    <input
+                      type={"checkbox"}
+                      checked={moves[k]}
+                      onChange={() =>
+                        updateMoves(
+                          Object.assign({}, moves, { [k]: !moves[k] })
+                        )
+                      }
+                    />
+                    {v}
+                  </label>
+                </div>
+              ))}
+            </div>
+            <div>
+              <div>speed: {speed.toFixed(2)}</div>
+              <div>
+                <button
+                  disabled={speed <= 0.2}
+                  onClick={() => updateSpeed(speed - 0.05)}
+                >
+                  ➖
+                </button>
+                <button
+                  disabled={speed >= 2}
+                  onClick={() => updateSpeed(speed + 0.05)}
+                >
+                  ➕
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+        <div>state: {move}</div>
+        <div style={{ flexGrow: 1, position: "relative" }}>
+          <Video
+            refObj={ref}
+            backupRef={backupRef}
+            prepRandom={prepRandom}
+            speed={speed}
+          />
+        </div>
+        <div style={{ display: "flex", justifyContent: "space-around" }}>
+          {["1", "2", "1+2"].map((k) => (
+            <div key={k}>
               <button
-                disabled={speed <= 0.2}
-                onClick={() => updateSpeed(speed - 0.05)}
+                style={{ padding: "1em", fontSize: "xx-large" }}
+                onClick={() => breakThrow(k)}
               >
-                ➖
-              </button>
-              <button
-                disabled={speed >= 2}
-                onClick={() => updateSpeed(speed + 0.05)}
-              >
-                ➕
+                {k}
               </button>
             </div>
-          </div>
-        </form>
-      </div>
-      <div>state: {move}</div>
-      <div style={{ flexGrow: 1, position: "relative" }}>
-        <Video refObj={ref} backupRef={backupRef} prepRandom={prepRandom} />
-      </div>
-      <div style={{ display: "flex", justifyContent: "space-around" }}>
-        {["1", "2", "1+2"].map((k) => (
-          <div key={k}>
-            <button
-              style={{ padding: "1em", fontSize: "xx-large" }}
-              onClick={() => {
-                lastButton = k;
-                breakThrow(k);
-              }}
-            >
-              {k}
-            </button>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
@@ -174,6 +177,7 @@ function Video(props: {
   refObj: React.RefObject<HTMLVideoElement>;
   backupRef: React.RefObject<HTMLVideoElement>;
   prepRandom: () => void;
+  speed: number;
 }) {
   useEffect(() => {
     if (initialized || !props.backupRef.current || !props.refObj.current)
@@ -204,6 +208,7 @@ function Video(props: {
         }}
         onCanPlay={(e) => {
           props.refObj.current!.src = (e.target as HTMLVideoElement).src;
+          props.refObj.current!.playbackRate = props.speed;
         }}
       ></video>
     </div>
