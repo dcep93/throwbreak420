14a6dad9e5815cc0aed57e0c63276a6af618cb4b
dcep93 <dcep93@gmail.com>
2024-02-04 05:11:49 -0500

    x

--- .github/workflows/build_react.sh
+++ .github/workflows/build_react.sh
@@ -5,6 +5,8 @@ set -euo pipefail
 # npx create-react-app throwbreak420 --template typescript
 # yarn add @babel/plugin-proposal-private-property-in-object --dev
 
+cd throwbreak420
+
 npm install
 yarn build
 rm -rf node_modules
