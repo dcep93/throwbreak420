4fde050ef21807eb61d5ef0746260fe2f3ed393e
dcep93 <dcep93@gmail.com>
2024-02-04 21:01:55 -0500

    x

--- throwbreak420/src/throwbreak420/Main.tsx
+++ throwbreak420/src/throwbreak420/Main.tsx
@@ -1,4 +1,4 @@
-import { createRef, useEffect, useState } from "react";
+import { ReactNode, createRef, useEffect, useState } from "react";
 
 const ALL_MOVES = {
   1: "1 break",
@@ -11,21 +11,17 @@ var initialzed = false;
 
 export default function Main() {
   console.log("main");
+  var throwBreak = "";
   const mainRef = createRef<HTMLVideoElement>();
   const backupRef = createRef<HTMLVideoElement>();
   const [shortcutToSet, updateShortcutToSet] = useState("");
   const [isP1, updateIsP1] = useState(true);
   const [isStanding, updateIsStanding] = useState(true);
-  const [speed, updateSpeed] = useState(1);
   const [possibleThrowBreaks, updatePossibleThrowBreaks] = useState(
     Object.fromEntries(Object.keys(ALL_MOVES).map((k) => [k, true]))
   );
-  const [throwBreak, updateThrowBreak] = useState("");
-  const [streak, updateStreak] = useState(0);
-  const [lastThrowBreak, updateLastThrowBreak] = useState("");
-  const [lastInput, updateLastInput] = useState("");
-  const [frame, updateFrame] = useState(0);
   var timeout: NodeJS.Timeout;
+
   const prepRandom = () => {
     console.log("prepRandom", initialzed);
     if (!initialzed) return;
@@ -38,181 +34,200 @@ export default function Main() {
     if (nextThrowBreak === undefined) {
       return;
     }
-    updateThrowBreak(nextThrowBreak);
+    throwBreak = nextThrowBreak;
     backupRef.current!.src = `video/${
       isStanding ? "standing" : "grounded"
-    }/${nextThrowBreak}.mkv#${Date.now()}`;
+    }/${throwBreak}.mkv#${Date.now()}`;
   };
-  useEffect(() => {
-    prepRandom();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [possibleThrowBreaks]);
-  const breakThrow = (button: string) => {
-    const video = mainRef.current;
-    if (!video) return;
-    const rawFrame = Math.floor(video.currentTime * 60);
-    const thisFrame = rawFrame - 66;
-    if (thisFrame < 0) return;
-    video.pause();
-    const fixedThrowBreak = throwBreak.replace("12", "1+2");
-    const incorrect = button !== fixedThrowBreak;
-    updateStreak(incorrect ? 0 : streak + 1);
-    updateLastThrowBreak(throwBreak);
-    updateLastInput(button);
-    updateFrame(thisFrame);
-    timeout = setTimeout(() => prepRandom(), incorrect ? 2000 : 500);
-  };
-  return (
-    <div
-      tabIndex={1}
-      onKeyDown={(e) => {
-        if (shortcutToSet !== "") {
-          shortcutToInput[e.key] = shortcutToSet;
-          updateShortcutToSet(
-            { "1": "2", "2": "1+2", "1+2": "" }[shortcutToSet]!
-          );
-          return;
-        }
-        var button =
-          { "1": "1", "2": "2", "3": "1+2" }[e.key] || shortcutToInput[e.key];
-        if (button === undefined) {
-          if (e.metaKey || !e.code.startsWith("Key")) return;
-          updateShortcutToSet("1");
-          return;
-        }
-        breakThrow(button);
-      }}
-    >
-      {shortcutToSet !== "" ? (
-        <div>set button {shortcutToSet}</div>
-      ) : (
-        <div
-          style={{
-            height: "100vH",
-            display: "flex",
-            flexDirection: "column",
-            fontFamily: "Courier New",
-            // TODO styling
-          }}
-        >
-          <div>
-            <form
-              style={{ display: "flex", justifyContent: "space-around" }}
-              onSubmit={(e) => e.preventDefault()}
-            >
-              <div>
+
+  var onEnded = () => {};
+
+  function Helper(props: { children: ReactNode }) {
+    console.log("helper");
+    const [speed, _updateSpeed] = useState(1);
+    const updateSpeed = (newSpeed: number) => {
+      const video = mainRef.current;
+      if (!video) return;
+      newSpeed = parseFloat(newSpeed.toFixed(2));
+      video.playbackRate = newSpeed;
+      _updateSpeed(newSpeed);
+    };
+    const [streak, updateStreak] = useState(0);
+    const [lastThrowBreak, updateLastThrowBreak] = useState("");
+    const [lastInput, updateLastInput] = useState("");
+    const [frame, updateFrame] = useState(0);
+    useEffect(() => {
+      prepRandom();
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [isP1, isStanding, possibleThrowBreaks]);
+    const breakThrow = (button: string) => {
+      const video = mainRef.current;
+      if (!video) return;
+      const rawFrame = Math.ceil(video.currentTime * 60);
+      const thisFrame = rawFrame - 66;
+      if (thisFrame < 0) return;
+      video.pause();
+      const fullThrowBreak = throwBreak.replace("12", "1+2");
+      const incorrect = button !== fullThrowBreak;
+      updateStreak(incorrect ? 0 : streak + 1);
+      updateLastThrowBreak(throwBreak);
+      updateLastInput(button);
+      updateFrame(thisFrame);
+      timeout = setTimeout(() => prepRandom(), incorrect ? 2000 : 500);
+    };
+    onEnded = () => breakThrow("-");
+    return (
+      <div
+        tabIndex={1}
+        onKeyDown={(e) => {
+          if (shortcutToSet !== "") {
+            shortcutToInput[e.key] = shortcutToSet;
+            updateShortcutToSet(
+              { "1": "2", "2": "1+2", "1+2": "" }[shortcutToSet]!
+            );
+            return;
+          }
+          var button =
+            { "1": "1", "2": "2", "3": "1+2" }[e.key] || shortcutToInput[e.key];
+          if (button === undefined) {
+            if (e.metaKey || !e.code.startsWith("Key")) return;
+            updateShortcutToSet("1");
+            return;
+          }
+          breakThrow(button);
+        }}
+      >
+        {shortcutToSet !== "" ? (
+          <div>set button {shortcutToSet}</div>
+        ) : (
+          <div
+            style={{
+              height: "100vH",
+              display: "flex",
+              flexDirection: "column",
+              fontFamily: "Courier New",
+              // TODO styling
+            }}
+          >
+            <div>
+              <form
+                style={{ display: "flex", justifyContent: "space-around" }}
+                onSubmit={(e) => e.preventDefault()}
+              >
                 <div>
-                  <label>
-                    <input
-                      type="radio"
-                      name="p1"
-                      checked={isP1}
-                      onChange={() => updateIsP1(true)}
-                    />
-                    p1
-                  </label>
-                </div>
-                <div>
-                  <label>
-                    <input
-                      disabled // TODO
-                      type="radio"
-                      name="p1"
-                      checked={!isP1}
-                      onChange={() => updateIsP1(false)}
-                    />
-                    p2
-                  </label>
-                </div>
-              </div>
-              <div>
-                <div>
-                  <label>
-                    <input
-                      type="radio"
-                      name="grounded"
-                      checked={isStanding}
-                      onChange={() => updateIsStanding(true)}
-                    />
-                    standing
-                  </label>
-                </div>
-                <div>
-                  <label>
-                    <input
-                      type="radio"
-                      name="grounded"
-                      checked={!isStanding}
-                      onChange={() => updateIsStanding(false)}
-                    />
-                    grounded
-                  </label>
-                </div>
-              </div>
-              <div>
-                {Object.entries(ALL_MOVES).map(([k, v]) => (
-                  <div key={k}>
+                  <div>
                     <label>
                       <input
-                        type={"checkbox"}
-                        checked={possibleThrowBreaks[k]}
-                        onChange={() =>
-                          updatePossibleThrowBreaks(
-                            Object.assign({}, possibleThrowBreaks, {
-                              [k]: !possibleThrowBreaks[k],
-                            })
-                          )
-                        }
+                        type="radio"
+                        name="p1"
+                        checked={isP1}
+                        onChange={() => updateIsP1(true)}
                       />
-                      {v}
+                      p1
                     </label>
                   </div>
-                ))}
-              </div>
-              <div>
-                <div>speed: {speed.toFixed(2)}</div>
+                  <div>
+                    <label>
+                      <input
+                        disabled // TODO
+                        type="radio"
+                        name="p1"
+                        checked={!isP1}
+                        onChange={() => updateIsP1(false)}
+                      />
+                      p2
+                    </label>
+                  </div>
+                </div>
                 <div>
+                  <div>
+                    <label>
+                      <input
+                        type="radio"
+                        name="grounded"
+                        checked={isStanding}
+                        onChange={() => updateIsStanding(true)}
+                      />
+                      standing
+                    </label>
+                  </div>
+                  <div>
+                    <label>
+                      <input
+                        type="radio"
+                        name="grounded"
+                        checked={!isStanding}
+                        onChange={() => updateIsStanding(false)}
+                      />
+                      grounded
+                    </label>
+                  </div>
+                </div>
+                <div>
+                  {Object.entries(ALL_MOVES).map(([k, v]) => (
+                    <div key={k}>
+                      <label>
+                        <input
+                          type={"checkbox"}
+                          checked={possibleThrowBreaks[k]}
+                          onChange={() =>
+                            updatePossibleThrowBreaks(
+                              Object.assign({}, possibleThrowBreaks, {
+                                [k]: !possibleThrowBreaks[k],
+                              })
+                            )
+                          }
+                        />
+                        {v}
+                      </label>
+                    </div>
+                  ))}
+                </div>
+                <div>
+                  <div>speed: {speed.toFixed(2)}</div>
+                  <div>
+                    <button
+                      disabled={speed <= 0.2}
+                      onClick={() => updateSpeed(speed - 0.05)}
+                    >
+                      ➖
+                    </button>
+                    <button
+                      disabled={speed >= 2}
+                      onClick={() => updateSpeed(speed + 0.05)}
+                    >
+                      ➕
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+            <div>
+              <div>throw: {lastThrowBreak}</div>
+              <div>input: {lastInput}</div>
+              <div>frame: {frame}</div>
+              <div>streak: {streak}</div>
+            </div>
+            <div style={{ flexGrow: 1, position: "relative" }}>
+              {props.children}
+            </div>
+            <div style={{ display: "flex", justifyContent: "space-around" }}>
+              {["1", "2", "1+2"].map((k) => (
+                <div key={k}>
                   <button
-                    disabled={speed <= 0.2}
-                    onClick={() => updateSpeed(speed - 0.05)}
+                    style={{ padding: "1em", fontSize: "xx-large" }}
+                    onClick={() => breakThrow(k)}
                   >
-                    ➖
-                  </button>
-                  <button
-                    disabled={speed >= 2}
-                    onClick={() => updateSpeed(speed + 0.05)}
-                  >
-                    ➕
+                    {k}
                   </button>
                 </div>
-              </div>
-            </form>
+              ))}
+            </div>
           </div>
-          <div>
-            <div>throw: {lastThrowBreak}</div>
-            <div>input: {lastInput}</div>
-            <div>frame: {frame}</div>
-            <div>streak: {streak}</div>
-          </div>
-          <div style={{ flexGrow: 1, position: "relative" }}>
-            <Video />
-          </div>
-          <div style={{ display: "flex", justifyContent: "space-around" }}>
-            {["1", "2", "1+2"].map((k) => (
-              <div key={k}>
-                <button
-                  style={{ padding: "1em", fontSize: "xx-large" }}
-                  onClick={() => breakThrow(k)}
-                >
-                  {k}
-                </button>
-              </div>
-            ))}
-          </div>
-        </div>
-      )}
-    </div>
-  );
+        )}
+      </div>
+    );
+  }
 
   function Video() {
     console.log("video");
@@ -230,7 +245,7 @@ export default function Main() {
           }}
           autoPlay
           muted
-          onEnded={() => breakThrow("-")}
+          onEnded={onEnded}
         ></video>
         <video
           src={`video/blank-0.2-sec.mkv`}
@@ -249,10 +264,15 @@ export default function Main() {
             }
             const video = mainRef.current!;
             video.src = (e.target as HTMLVideoElement).src;
-            video.playbackRate = speed; // TODO make not necessary cuz we dont rerender!
+            // video.playbackRate = speed; // TODO make not necessary cuz we dont rerender!
           }}
         ></video>
       </div>
     );
   }
+  return (
+    <Helper>
+      <Video />
+    </Helper>
+  );
 }
