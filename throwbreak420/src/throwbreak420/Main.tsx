0e8dccf054af4cae2c503daff09273ae6453411a
dcep93 <dcep93@gmail.com>
2024-02-12 15:04:01 -0500

    x

--- throwbreak420/src/throwbreak420/Main.tsx
+++ throwbreak420/src/throwbreak420/Main.tsx
@@ -128,119 +128,158 @@ export default function Main() {
         ) : (
           <div
             style={{
-              height: "100vH",
-              width: "100vW",
-              display: "flex",
-              flexDirection: "column",
               fontFamily: "Courier New",
               color: "#f3f3f8",
               backgroundColor: "#282a3a",
             }}
           >
-            <div>
-              <form
-                style={{ display: "flex", justifyContent: "space-around" }}
-                onSubmit={(e) => e.preventDefault()}
-              >
-                <div>
+            <div
+              style={{
+                height: "100vH",
+                width: "100vW",
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <div>
+                <form
+                  style={{ display: "flex", justifyContent: "space-around" }}
+                  onSubmit={(e) => e.preventDefault()}
+                >
                   <div>
-                    <label>
-                      <input
-                        type="radio"
-                        name="p1"
-                        checked={isP1}
-                        onChange={() => updateIsP1(true)}
-                      />
-                      p1
-                    </label>
-                  </div>
-                  <div>
-                    <label onClick={() => alert("not implemented")}>
-                      <input
-                        disabled // TODO
-                        type="radio"
-                        name="p1"
-                        checked={!isP1}
-                        onChange={() => updateIsP1(false)}
-                      />
-                      p2
-                    </label>
-                  </div>
-                </div>
-                <div>
-                  {Object.keys(ALL_MOVES).map((c) => (
-                    <div key={c}>
+                    <div>
                       <label>
                         <input
                           type="radio"
-                          name="category"
-                          checked={c === category}
-                          onChange={() => updateCategory(c)}
+                          name="p1"
+                          checked={isP1}
+                          onChange={() => updateIsP1(true)}
                         />
-                        {c}
+                        p1
                       </label>
                     </div>
-                  ))}
-                </div>
-                <div>
-                  {Object.entries(possibles).map(([k, v]) => (
-                    <div key={k}>
-                      <label>
+                    <div>
+                      <label onClick={() => alert("not implemented")}>
                         <input
-                          type={"checkbox"}
-                          checked={v}
-                          onChange={() =>
-                            updatePossibles(
-                              Object.assign({}, possibles, {
-                                [k]: !v,
-                              })
-                            )
-                          }
+                          disabled // TODO
+                          type="radio"
+                          name="p1"
+                          checked={!isP1}
+                          onChange={() => updateIsP1(false)}
                         />
-                        {ALL_MOVES[category][k].name}
+                        p2
                       </label>
                     </div>
-                  ))}
-                </div>
-                <div>
-                  <div>speed: {_speed.toFixed(2)}</div>
+                  </div>
                   <div>
+                    {Object.keys(ALL_MOVES).map((c) => (
+                      <div key={c}>
+                        <label>
+                          <input
+                            type="radio"
+                            name="category"
+                            checked={c === category}
+                            onChange={() => updateCategory(c)}
+                          />
+                          {c}
+                        </label>
+                      </div>
+                    ))}
+                  </div>
+                  <div>
+                    {Object.entries(possibles).map(([k, v]) => (
+                      <div key={k}>
+                        <label>
+                          <input
+                            type={"checkbox"}
+                            checked={v}
+                            onChange={() =>
+                              updatePossibles(
+                                Object.assign({}, possibles, {
+                                  [k]: !v,
+                                })
+                              )
+                            }
+                          />
+                          {ALL_MOVES[category][k].name}
+                        </label>
+                      </div>
+                    ))}
+                  </div>
+                  <div>
+                    <div>speed: {_speed.toFixed(2)}</div>
+                    <div>
+                      <button
+                        disabled={speed <= 0.2}
+                        onClick={() => updateSpeed(speed - 0.05)}
+                      >
+                        ➖
+                      </button>
+                      <button
+                        disabled={speed >= 2}
+                        onClick={() => updateSpeed(speed + 0.05)}
+                      >
+                        ➕
+                      </button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+              <div style={{ paddingLeft: "2em" }}>
+                <div>answer: {lastAnswer}</div>
+                <div>input: {lastInput}</div>
+                <div>frame: {frame}</div>
+                <div>streak: {streak}</div>
+              </div>
+              <div style={{ flexGrow: 1, position: "relative" }}>
+                {props.children}
+              </div>
+              <div style={{ display: "flex", justifyContent: "space-around" }}>
+                {["1", "2", "1+2"].map((k) => (
+                  <div key={k}>
                     <button
-                      disabled={speed <= 0.2}
-                      onClick={() => updateSpeed(speed - 0.05)}
+                      style={{ padding: "1em", fontSize: "xx-large" }}
+                      onClick={() => handleInput(k)}
                     >
-                      ➖
-                    </button>
-                    <button
-                      disabled={speed >= 2}
-                      onClick={() => updateSpeed(speed + 0.05)}
-                    >
-                      ➕
+                      {k}
                     </button>
                   </div>
-                </div>
-              </form>
+                ))}
+              </div>
             </div>
-            <div style={{ paddingLeft: "2em" }}>
-              <div>answer: {lastAnswer}</div>
-              <div>input: {lastInput}</div>
-              <div>frame: {frame}</div>
-              <div>streak: {streak}</div>
-            </div>
-            <div style={{ flexGrow: 1, position: "relative" }}>
-              {props.children}
-            </div>
-            <div style={{ display: "flex", justifyContent: "space-around" }}>
-              {["1", "2", "1+2"].map((k) => (
-                <div key={k}>
-                  <button
-                    style={{ padding: "1em", fontSize: "xx-large" }}
-                    onClick={() => handleInput(k)}
+            <div style={{ overflow: "hidden", padding: "2em" }}>
+              <h2
+                onClick={() => {
+                  // @ts-ignore
+                  window.location.reload(true);
+                }}
+              >
+                ThrowBreak420
+              </h2>
+              <ul>
+                <li>
+                  welcome! check out{" "}
+                  <a
+                    style={{ color: "#f3f3f8" }}
+                    href="https://www.youtube.com/watch?v=BDUUbuzuelA"
                   >
-                    {k}
-                  </button>
-                </div>
-              ))}
+                    this
+                  </a>{" "}
+                  video from PhiDX for pro tips
+                </li>
+                <li>practice throw breaks on any browser, even mobile</li>
+                <li>control speed</li>
+                <li>see which frame you pressed - were you close?</li>
+                <li>record your streak</li>
+                <li>this app is still pre-release</li>
+                <li>
+                  TODO{" "}
+                  <ul>
+                    <li>p1/p2</li>
+                    <li>random intervals</li>
+                  </ul>
+                </li>
+              </ul>
             </div>
           </div>
         )}
