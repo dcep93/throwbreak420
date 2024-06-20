16fd482536a9da2fc0d3dc50eb502212bc3eb747
dcep93 <dcep93@gmail.com>
2024-06-19 17:13:02 -0700

    x

--- throwbreak420/src/throwbreak420/Buttons.tsx
+++ throwbreak420/src/throwbreak420/Buttons.tsx
@@ -5,17 +5,35 @@ export default function Buttons(props: {
   };
 }) {
   return (
-    <div style={{ display: "flex", justifyContent: "space-around" }}>
-      {Object.keys(props.get().possibles).map((k) => (
-        <div key={k}>
-          <button
-            style={{ padding: "1em", fontSize: "xx-large" }}
-            onClick={() => props.get().handleInput(k)}
-          >
-            {k}
-          </button>
+    <div style={{ display: "flex", backgroundColor: "black" }}>
+      <div
+        style={{
+          margin: "auto",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            margin: "20px ",
+          }}
+        >
+          {Object.keys(props.get().possibles).map((k) => (
+            <div key={k}>
+              <button
+                style={{
+                  padding: "1em",
+                  borderRadius: "100%",
+                  fontSize: "xx-large",
+                  margin: "0 25px",
+                }}
+                onClick={() => props.get().handleInput(k)}
+              >
+                {k}
+              </button>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
     </div>
   );
 }
